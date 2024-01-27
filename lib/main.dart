9732bdf7e6c720b9919ea542ea6973216875b60a
PraveenJJ <praveen_jj123@yahoo.com>
2024-01-27 15:56:05 +0530

    form for login page 1.1

--- lib/main.dart
+++ lib/main.dart
@@ -38,34 +38,103 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            TextField(
+        appBar: null,
+        body: Container(
+          child: const Column(
+            children: [
+              // for logo and welcome
+              WelcomeLogoWidget(),
+              // for sign-in form
+              SignInForm(),
+            ],
+          ),
+        ));
+  }
+}
+
+class WelcomeLogoWidget extends StatefulWidget {
+  const WelcomeLogoWidget({super.key});
+  @override
+  State<StatefulWidget> createState() => WelcomeLogoWidgetState();
+}
+
+class WelcomeLogoWidgetState extends State<WelcomeLogoWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
+
+class SignInForm extends StatefulWidget {
+  const SignInForm({super.key});
+
+  @override
+  SignInFormState createState() => SignInFormState();
+}
+
+class SignInFormState extends State<SignInForm> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _submitForm() async {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            width: 320,
+            padding: const EdgeInsets.fromLTRB(15.0, 0.0, 0.0, 0.0),
+            decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12)),
+            child: TextField(
               controller: _usernameController,
               decoration: const InputDecoration(
                 labelText: 'Username',
+                border: InputBorder.none,
               ),
             ),
-            const SizedBox(height: 20.0),
-            TextField(
+          ),
+          const SizedBox(height: 20.0),
+          Container(
+            width: 320,
+            padding: const EdgeInsets.fromLTRB(15.0, 0.0, 0.0, 0.0),
+            decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12)),
+            child: TextField(
               controller: _passwordController,
               obscureText: true,
               decoration: const InputDecoration(
                 labelText: 'Password',
+                border: InputBorder.none,
               ),
             ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: _submitForm,
-              child: const Text('Sign In'),
+          ),
+          const SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: _submitForm,
+            style: ElevatedButton.styleFrom(
+              primary: Colors.deepPurple,
             ),
-          ],
-        ),
+            child: const Text(
+              'Sign In',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18),
+            ),
+          ),
+        ],
       ),
     );
   }
