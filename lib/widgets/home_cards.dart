fc963b538812e3b9dc75711c16159fdd6306acd6
PraveenJJ <praveen_jj123@yahoo.com>
2024-02-15 19:30:58 +0530

    Dynamic doctor list rendering 1.0

--- lib/widgets/home_cards.dart
+++ lib/widgets/home_cards.dart
@@ -1,82 +1,46 @@
+import 'package:doc_scheduler_app/widgets/doctor_cards.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomeCards extends StatelessWidget {
-
-
-
+class HomeCards extends StatefulWidget {
   const HomeCards({super.key});
+
+  @override
+  HomeCardsState createState() => HomeCardsState();
+}
+
+class HomeCardsState extends State<HomeCards> {
+  List<dynamic> data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData(); // Fetch data when the page initializes
+  }
+
+  Future<void> fetchData() async {
+    // Load JSON file
+    String jsonContent =
+        await rootBundle.loadString('assets/resources/doctors.json');
+    List<dynamic> doctors = json.decode(jsonContent);
+
+    setState(() {
+      data = doctors; // Update the data variable with fetched data
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-        children: [
-          const SizedBox(height: 15),
-          Container(
-            height: 130,
-            width: 330,
-            padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-            child: Card(
-              child: Column(
-                children: <Widget>[
-                  const ListTile(
-                    leading: Icon(Icons.local_hospital),
-                    title: Text('Dr. Praveen Jaishankar'),
-                    subtitle: Text('Specialist in Psychology'),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TextButton(
-                        child: const Text('Book Now'),
-                        onPressed: () {},
-                      ),
-                      const SizedBox(width: 8),
-                      TextButton(
-                        child: const Text('Show Treatments'),
-                        onPressed: () {/* ... */},
-                      ),
-                      const SizedBox(width: 8),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 15),
-          Container(
-            height: 130,
-            width: 330,
-            padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-            child: Card(
-              child: Column(
-                children: <Widget>[
-                  const ListTile(
-                    leading: Icon(Icons.local_hospital),
-                    title: Text('Dr. Swathi Vadivelu'),
-                    subtitle: Text('Specialist in Pediatrician'),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TextButton(
-                        child: const Text('Book Now'),
-                        onPressed: () {},
-                      ),
-                      const SizedBox(width: 8),
-                      TextButton(
-                        child: const Text('Show Treatments'),
-                        onPressed: () {/* ... */},
-                      ),
-                      const SizedBox(width: 8),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-      )
+        children: data.map<Widget>((doctor) {
+          return DoctorCards(
+              title: doctor['doctorName'] as String,
+              subtitle: doctor['speciality'] as String);
+        }).toList(),
+      ),
     );
   }
 }
